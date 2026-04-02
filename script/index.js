c1649e168423bdf26dff96b61621eebf263e80ea
mdmirajhossansajid <mdmirajhossansajidpc@gmail.com>
2026-04-02 08:56:27 +0600

    add new section lessons

---
+++ script/index.js
@@ -0,0 +1,16 @@
+const loadData=()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then(res=>(res.json()))
+    .then(data=>displayData(data.data));
+}
+const displayData=(lessons)=>{
+   const levelContainer=document.getElementById("level-container");
+   levelContainer.innerHTML="";
+   for(let lesson of lessons){
+    const btnDiv=document.createElement("div");
+    btnDiv.innerHTML=`
+   <button  class="btn btn-soft btn-primary"><i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no}</button>`
+    levelContainer.appendChild(btnDiv);
+   }
+}
+loadData();
